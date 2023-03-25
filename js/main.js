d9e21fe2bc306c5d29b0adeb704789ac0a2c38cf
tatiana maksimovich <tatianamaksimovich@gmail.com>
2023-03-25 14:16:35 +0300

    Помощь друга

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import {createPhotos} from './data.js';
 import {renderPictures} from './thumbnail.js';
 import './form.js';
+import './scale.js';
+import './effect.js';
 
 const photos = createPhotos();
 
